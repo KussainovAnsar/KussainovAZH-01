0f9f19f20bd6c67b56190098ab4f1e170582eecf
Ансар <119420509+KussainovAnsar@users.noreply.github.com>
2024-02-18 18:37:07 +0600

    Create postgres.go

---
+++ pkg/store/postgres/postgres.go
@@ -0,0 +1,26 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+  "github.com/KussainovAnsar/day-1-2-3/new/KussainovAZH-02/pkg/postgres"
+)
+
+func ConnectDB(host, port, user, password, dbname string) (*sql.DB, error) {
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
