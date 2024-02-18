0563f83362d7335250e3e9801fcac327040a4dc9
KussainovAnsr <Ansar02012004@gmail.com>
2024-02-18 19:30:40 +0600

    Add usecase.go file

--- services/contact/internal/usecase/usecase.go
+++ services/contact/internal/usecase/usecase.go
@@ -1,1 +1,14 @@
- 
+package useсase
+
+type ContactUseCase interface {
+	CreateContact() error
+	ReadContact() error
+	UpdateContact() error
+	DeleteContact() error
+}
+
+type GroupUseCase interface {
+	CreateGroup() error
+	ReadGroup() error
+	AddContactToGroup() error
+}
